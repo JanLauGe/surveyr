f7cb7a6e864cdc0c5419156ccc1bf7e2fbfd39c6
JanLauGe <laurensgeffert@gmail.com>
2018-04-24 17:55:59 +0100

    working survey object, prototyping response data

--- R/survey-gizmo_experiments.R
+++ R/survey-gizmo_experiments.R
@@ -3,105 +3,328 @@ library(stringr)
 library(forcats)
 library(httr)
 library(jsonlite)
-
-# Get API key from dotfile.
-# Should be saved as: 
-# survey-gizmo_api-token=[...]
-# survey-gizmo_api-secret=[...]
-api_token <- read_file('~/.ssh/.dotfile') %>% 
-  str_split(pattern = '\n') %>% unlist() %>%
-  str_split(pattern = '=', simplify = TRUE) %>% 
-  {.[,2]} %>% {.[. != '']}
-
-# Test survey
-survey_id = '4274510' 
+library(lubridate)
 
 
 # API call functions -----------------------------------------------------------
 
+sg_get_api_token <- function() {
+  #' Get API key from dotfile.
+  #' Should be saved as: 
+  #' survey-gizmo_api-token=[...]
+  #' survey-gizmo_api-secret=[...]
+  api_token <- read_file('~/.ssh/.dotfile') %>% 
+    str_split(pattern = '\n') %>% unlist() %>%
+    str_split(pattern = '=', simplify = TRUE) %>% 
+    {.[,2]} %>% {.[. != '']}
+  return(api_token)
+}
+
+
+sg_apicall <- function(
+  # survey id as string
+  survey_id,
+  # API information as list of length 2 (token and secret)
+  api_token,
+  # e.g. 'surveyquestion' or 'surveyresponse'
+  api_object_class,
+  api_url = 'https://restapi.surveygizmo.com',
+  api_version = 'v5',
+  api_calltype = 'survey') {
+  
+  #' Generates and executes an httr call to the surveygismo REST API.
+  #' Takes survey_id, api_token, api_object to generate path.
+  #' Assumes default version 5.
+  #' 
+  #' Further work to be done to access different kinds of objects,
+  #' for example account information etc.
+  
+  # generate httr call
+  # with surveyid
+  response <- GET(
+    url = api_url,
+    path = paste(
+      api_version,
+      api_calltype,
+      survey_id,
+      api_object_class,
+      sep = '/'),
+    query = list(
+      api_token = api_token[1],
+      api_token_secret = api_token[2],
+      resultsperpage = 50),
+    progress())
+  
+  # check response
+  stop_for_status(response)
+  
+  # parse response
+  out <- content(response, as = 'text') %>% 
+    jsonlite::fromJSON()
+  return(out)
+}
+
+
+# Function to standardise question options
+sg_clean_options <- function(df) {
+  if (nrow(df) > 0) {
+    out <- df %>% 
+      ### TODO: Allow for different languages
+      #select(id, value, starts_with('title')) %>%
+      #select(1,2,3) %>%
+      #set_names(c('o_id','o_value','o_text'))
+      transmute(
+        o_id = id,
+        o_value = value
+      )
+  } else {
+    out <- data.frame(
+      o_id = NA,
+      o_value = NA)
+  }
+  return(out)
+}
+
+
 # get survey
-sg_get_questions <- function(
+sg_get_survey <- function(
   survey_id,
   api_token) {
-  #' This function takes a survey id and api token
+  #' Takes a survey id and api token
   #' to retrieve a dataframe of questions in the given survey
   #' along with question id and question type
   
-  # generate httr call for survey subobject
-  # with surveyid
-  response <- GET(
-    url = 'https://restapi.surveygizmo.com',
-    path = paste('v5/survey', surveyid, 'surveyquestion', sep = '/'),
-    query = list(
-      api_token = api_token[1],
-      api_token_secret = api_token[2]),
-    progress())
-  # check response
-  warn_for_status(response)
-  stop_for_status(response)
-  # parse response
-  x <- content(response, as = 'text') %>% 
-    jsonlite::fromJSON()
-  
-  # assumes english responses for now
-  ### TODO: allow for language selection
+  # get data from REST API
+  response <- sg_apicall(
+    survey_id = survey_id,
+    api_token = api_token,
+    api_object_class = 'surveyquestion')
   
   # get question information
   sg_questions <- data_frame(
     # question ID
-    q_id = x$data$id,
+    q_id = response$data$id,
     # question type, e.g. checkbox, radio
-    q_type = x$data$`_subtype`,
+    q_type = response$data$type,
     # question text without HTML tags
-    q_text = x$data$title$English %>%
+    q_text = response$data$title$English %>%
       str_replace_all(pattern = '<.*?>', replacement = ''))
   
-  return(sg_questions)
+  # get option information
+  sg_survey <- response$data$options %>% 
+    # make lists of lists into dataframes
+    map(flatten) %>%
+    # standardise formatting
+    map(sg_clean_options) %>%
+    # make into list of dataframes
+    enframe(name = 'index', value = 'options') %>%
+    select(options) %>%
+    # append to question information
+    bind_cols(sg_questions, .) %>%
+    # one row per option
+    unnest(options) %>%
+    filter(!is.na(o_id)) %>%
+    # create option labels
+    mutate(o_tag = str_c(
+      'Q', str_pad(q_id, width = max(nchar(.$q_id)), pad = 0), '_',
+      'O', str_pad(o_id, width = max(nchar(.$o_id)), pad = 0))) %>%
+    select(
+      q_id, q_type, q_text,
+      o_id, o_tag, o_value)
+  
+  return(sg_survey)
 }
+
+
+sg_relabel_answers <- function(question_index, df_answers, df_survey) {
+  ### TODO: Double-check that this returns correct factor levels
+  assign('qi', question_index, envir = .GlobalEnv)
+  
+  # test question type
+  df_question <- filter(df_survey, q_id == question_index)
+  q_type <- unique(df_question$q_type)
+  
+  # check that column was found
+  if ((df_answers %>% select(starts_with(question_index)) %>% ncol) == 0) {
+    out <- rep(NA, nrow(df_answers))
+    return(out)
+  } else if (df_answers %>% 
+             select(matches(str_c('^', question_index, '.*shown$'))) %>% 
+             unlist %>% {!any(.)}) {
+    out <- rep(NA, nrow(df_answers))
+    return(out)
+    
+  } else if (q_type %in% c('RADIO', 'MENU')) {
+    if (!str_c(question_index, '.answer_id') %in% colnames(df_answers)) {
+      out <- rep(NA, nrow(df_answers))
+      return(out)
+    } else {
+      # extract answers vector
+      x <- df_answers %>% 
+        pull(str_c(question_index, '.answer_id')) %>%
+        as.character %>%
+        as.factor
+      # get label lookup vector
+      str_labels <- pull(df_question, o_value)
+      names(str_labels) <- pull(df_question, o_id)
+      # execute recoding
+      out <- do.call(dplyr::recode, c(list(x), str_labels))
+      return(out)
+    }
+  } else if (q_type == 'CHECKBOX') {
+    # get selected option from multiple choice question
+    x <- df_answers %>%
+      select(matches(str_c('^', question_index, '\\..*option$')))
+    # get index number of options
+    option_index <- colnames(x) %>% 
+      str_split('\\.', simplify = TRUE) %>% {.[,3]}
+    # set names with a string generated from question and option index
+    out <- set_names(x, str_c(
+      'Q', str_pad(question_index, width = 3, pad = 0),
+      'O', str_pad(option_index, width = 5, pad = 0)))
+    return(out)
+  }
+}
+
+
+sg_relabel_answers <- function(question_index, df_answers, df_survey) {
+  ### TODO: Double-check that this returns correct factor levels
+  assign('qi', question_index, envir = .GlobalEnv)
+  
+  # test that answer data is valid
+  # check that column was found
+  if ((df_answers %>% select(starts_with(question_index)) %>% ncol) == 0) {
+    out <- rep(NA, nrow(df_answers)); return(out)}
+  if (df_answers %>% select(matches(str_c('^', question_index, '.*shown$'))) %>% 
+    unlist %>% {!any(.)}) {
+    out <- rep(NA, nrow(df_answers)); return(out)}
+  
+  # test question type from df_survey
+  df_question <- filter(df_survey, q_id == question_index)
+  
+  
+  
+  
+  
+  # TODO: continue here
+  
+  # test question type from df_answers
+  q_type <- select(df_answers, starts_with(str_c(question_index, '.type')))
+  if (ncol(q_type) == 0) {
+    q_type <- 'unknown'
+  } else {
+    q_type <- q_type %>% filter(complete.cases(.)) %>% pull(1) %>% unique
+  }
+  #'TABLE', 'IMAGE_SELECT', 'MULTI_TEXTBOX', 'RANK', 'HIDDEN', 'parent'
+  print(q_type)
+
+  
+  
+  
+  # Single choice questions
+  if (q_type %in% c('RADIO', 'MENU')) {
+    # extract answers vector
+    x <- df_answers %>% 
+      pull(str_c(question_index, '.answer_id')) %>%
+      as.character %>%
+      as.factor
+    # get label lookup vector
+    str_labels <- pull(df_question, o_value)
+    names(str_labels) <- pull(df_question, o_id)
+    # execute recoding
+    out <- do.call(dplyr::recode, c(list(x), str_labels))
+    return(out)
+  
+  # Multiple choice questions
+  } else if (q_type %in% c('CHECKBOX')) {
+    x <- df_answers %>%
+      select(matches(str_c('^', question_index, '\\..*option$')))
+    # get index number of options
+    option_index <- colnames(x) %>% 
+      str_split('\\.', simplify = TRUE) %>% {.[,3]}
+    # set names with a string generated from question and option index
+    out <- set_names(x, str_c(
+      'Q', str_pad(question_index, width = 3, pad = 0),
+      'O', str_pad(option_index, width = 5, pad = 0)))
+    return(out)
+  
+  # Hierarchical questions
+  } else if (q_type %in% c('parent')) {
+    out <- rep(NA, nrow(df_answers))
+    return(out)
+  }
+}
+
+
+
+
+
+
 
 
 # get responses
 sg_get_responses <- function(
   survey_id,
   api_token) {
-  #' This function takes a survey id and api token
+  #' This function takes a survey id, api token, and a list of questions
   #' to retrieve a dataframe of responses to the given survey
   
-  # generate httr call
-  # with surveyid
-  response <- GET(
-    url = 'https://restapi.surveygizmo.com',
-    path = paste('v5/survey', surveyid, 'surveyresponse', sep = '/'),
-    query = list(
-      api_token = api_token[1],
-      api_token_secret = api_token[2]),
-    progress())
-  # check response
-  warn_for_status(response)
-  stop_for_status(response)
-  # parse response
-  x <- content(response, as = 'text') %>% 
-    jsonlite::fromJSON()
-
+  # get survey structure from REST API
+  df_survey <- sg_get_survey(survey_id, api_token)
+  
+  # get response data from REST API
+  api_resp <- sg_apicall(
+    survey_id = survey_id,
+    api_token = api_token,
+    api_object_class = 'surveyresponse')
+  
+  # transform participant level general information
+  df_participants <- api_resp$data %>%
+    select(which(!map(api_resp$data, class) %in% c('list', 'data.frame'))) %>%
+    as_tibble() %>%
+    transmute(
+      resp_id = id,
+      resp_status = status,
+      resp_test = is_test_data,
+      resp_time_started = date_started %>% lubridate::as_datetime(),
+      resp_time_finished = date_submitted %>% lubridate::as_datetime(),
+      resp_time_diff = response_time,
+      resp_linkid = link_id,
+      resp_referrer = referer,
+      resp_useragent = user_agent,
+      resp_country = country,
+      resp_city = city,
+      resp_region = region,
+      # resp_postcode = postal,
+      # resp_lat = latitude,
+      # resp_lon = longitude,
+      # resp_ip = ip_address,
+      resp_dma = dma)
+  
+  # transform question level information
+  df_answers <- api_resp$data$survey_data %>% flatten()
+  
+  # question ids
+  i_questions <- df_survey$q_id %>% unique %>% as.character
+  
+  bla <- map(
+    .x = i_questions,
+    .f = sg_relabel_answers,
+    df_answers,
+    df_survey)
+  
+  
+  
+  
+  
+  
+  
+  # bind participant info and answers
+  
+  return(df_responses)
 }
 
 
-
-
-
-
-# Get survey level info
-df_survey <- with(x$data,
-  data_frame(
-    sur_id = id,
-    sur_title = title,
-    sur_type = type,
-    sur_forward_only = forward_only,
-    sur_date_created = created_on %>% lubridate::as_datetime(),
-    sur_date_modified = modified_on %>% lubridate::as_datetime(),
-    sur_status = status
-  )
-)
 
 
 
@@ -113,35 +336,105 @@ df_survey <- with(x$data,
 
 # Testing ----------------------------------------------------------------------
 
+# Get API token and secret from .dotfile
+api_token <- sg_get_api_token()
+
+survey_id = '4253549' # Banking survey
+survey_id = '4274510' # Test survey
+
+
+# %% get survey --------------------
 # run function to retrieve questions
-df_questions <- sg_get_questions(
+df_survey <- sg_get_survey(
   survey_id = survey_id,
   api_token = api_token)
 
+# # Single choice vs multiple choice
+# q_type_singlechoice <- c('RADIO', 'MENU', 'NPS')
+# q_type_multiplechoice <- c('CHECKBOX', 'RANK')
+# 
+# df_single_choice <- df_survey %>%
+#   filter(q_type %in% q_type_singlechoice)
+# 
+# df_multiple_choice <- df_survey %>%
+#   filter(q_type %in% q_type_multiplechoice)
+
+
+xx <- df_survey %>% 
+  nest(starts_with('o'), .key = 'options')
+  
+sg_relabel_answers(x, '5', df_survey)
+
+
+
+# %% get responses -------------------------------------------------------------
 
 
 
 
-surveyid = '4253549' # Banking survey
-surveyid = '4274510' # Test survey
 
 
-# generate httr call
-response <- GET(
-  url = 'https://restapi.surveygizmo.com/v5/survey', 
-  query = list(
-    api_token = api_token[1],
-    api_token_secret = api_token[2]))
+# Code bin -------------------------
 
-# check response
-warn_for_status(response)
-stop_for_status(response)
 
-# parse response using jsonlite
-x <- content(response, as = 'text') %>% 
-  fromJSON(flatten = FALSE)
 
-x$data %>% as_data_frame()
+
+# # get ranking where appropriate
+# bind_cols(
+#   'option' = select(out, -ends_with('.rank')) %>% t,
+#   'rank' = select(out, ends_with('.rank')) %>% t)
+
+# 
+# 
+# 
+# # bla <- function(q_id, response) {
+# #   # convert numeric q_id to character
+# #   q_id <- as.character(q_id)
+# #   # use q_id to extract raw responses to question
+# #   x <- response$data$survey_data[as.character(q_id)]
+# #   
+# #   
+# #   return(x)
+# # }
+# 
+# 
+# 
+# map(.x = qs, .f = bla, x)
+# 
+# x$data$survey_data
+# 
+# 
+# x$data$survey_data %>%
+#   pull('31') %>%
+#   flatten() %>%
+#   select(id, starts_with('answer'))
+# 
+# 
+# tibble(
+#   response_id = x$data$id,
+#   response_status = x$data$status,
+#   response_test = x$data$
+# )
+# 
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ pullsg_excerpt.R
@@ -0,0 +1,200 @@
+library(tidyverse)
+library(stringr)
+library(forcats)
+library(httr)
+library(jsonlite)
+
+# Get API key from dotfile.
+# Should be saved as: 
+# survey-gizmo_api-token=[...]
+# survey-gizmo_api-secret=[...]
+api_token <- read_file('~/.ssh/.dotfile') %>% 
+  str_split(pattern = '\n') %>% unlist() %>%
+  str_split(pattern = '=', simplify = TRUE) %>% 
+  {.[,2]} %>% {.[. != '']}
+
+surveyid = '4253549'
+api =  paste0(api_token[1], '&api_token_secret=', api_token[2])
+completes_only = TRUE
+verbose = TRUE
+var_name_append = TRUE
+mergecampaign = FALSE
+delete_sys_vars = FALSE
+keep_geo_vars = TRUE
+clean = FALSE
+reset_row_names = TRUE
+small = FALSE
+
+
+
+# From Rsurveygizmo package ----------------------------------------------------
+
+options(stringsAsFactors=FALSE)
+
+if(small & mergecampaign==FALSE) warning('\nThe "small" parameter should be false when "mergecampaign" is false. This parameter was ignored.')
+# Set hard-coded URL parameters
+token <- paste0('?api_token=', api) # Must be in the first trailing URL position
+url      <- 'https://restapi.surveygizmo.com/v4/survey/'
+response <- "/surveyresponse/"
+question <- "/surveyquestion/"
+pages    <- "&page="
+results  <- "&resultsperpage=100"
+
+#Build local parameters
+filturl  <- paste0("&filter[field][0]=status&filter[operator][0]==",
+                   "&filter[value][0]=Complete")
+if (completes_only) {
+  filt = filturl
+} else {
+  filt <- ""
+}
+
+lc_base  <- paste0(url, surveyid, response, token, filt)
+lc_furl  <- paste0(url, surveyid, response, token, results, filt, pages)
+lc_qurl  <- paste0(url, surveyid, question, token)
+
+message("\n  Retrieving survey summary data:")
+progb <- txtProgressBar(min = 0, max = 4, style = 3)
+setTxtProgressBar(progb, 1)
+
+# Get base response parameters of the survey and extract N
+lc_base      <- fromJSON(txt=lc_base)
+lc_resp_cnt  <- as.integer(lc_base[['total_count']])
+setTxtProgressBar(progb, 2)
+
+# Calculate page number (starting with 1) based on 100 responses per call
+lc_respnum  <- ceiling(lc_resp_cnt/100)
+
+# Retrieve the question list from the "/surveyquestion/" call
+lc_qs   <- jsonlite::fromJSON(txt=lc_qurl)
+setTxtProgressBar(progb, 3)
+
+lc_qs   <- as.data.frame(lc_qs$data)
+
+# Extract question text for questions without a defined SG alias, stripping
+# html tags, punctuation, and spaces, keeping the first 35 characters and
+# appending the question id.
+lc_qs$qtext <- gsub("<.*?>", "", lc_qs$title$English)
+lc_qs$qtext <- gsub("[[:punct:]]", "", lc_qs$qtext)
+setTxtProgressBar(progb, 4)
+
+if(var_name_append) {
+  lc_qs$qtext <- paste0(substr(gsub("[[:space:]]", ".", lc_qs$qtext), 1, 35),
+                        "_ID", lc_qs$id)
+  lc_qs$qtext <- trimws(lc_qs$qtext)
+} else {
+  lc_qs$qtext <- paste0(substr(gsub("[[:space:]]", ".", lc_qs$qtext), 1, 75))
+  lc_qs$qtext <- trimws(lc_qs$qtext)
+}
+
+lc_qs$shortname <- ifelse(is.na(lc_qs$shortname) | lc_qs$shortname=="",
+                          lc_qs$qtext, lc_qs$shortname)
+close(progb)
+
+# Drop instructional messages and subset the frame, keeping shortname and id.
+lc_qs <- lc_qs[lc_qs$`_type` !="SurveyDecorative", c('id', 'shortname')]
+
+# Retrieve the response data with the "/surveyresponse/" call
+### &resultsperpage=100&filter[field][0]=status&filter[operator][0]==&filter[value][0]=Complete&page=1"
+message("\n  Retrieving survey response data:")
+progb <- txtProgressBar(min = 0, max = length(lc_respnum), style = 3)
+for(i in 1:lc_respnum){
+  sg_return_url  <- paste0(lc_furl, i)
+  sg_return_data <- fromJSON(txt=sg_return_url)
+  sg_return_data <- as.data.frame(sg_return_data$data)
+  assign(paste0("lc_survey_page", i), sg_return_data)
+  setTxtProgressBar(progb, i)
+}
+close(progb)
+
+#Bind the returned frames
+lc_fullset <- do.call("rbind", mget(ls(pattern="lc_survey_page")))
+
+# Extract the column names of the responsedata returned from SG
+lc_names <- names(lc_fullset)
+
+# Define patterns to drop from the vector of column names
+patterns = "[[:punct:]]|[[:space:]]|question|shown|STANDARD"
+
+# Hidden values generally take form of: 'question(76), option(0)'
+# If a hidden value is assigned to more than one field (e.g., in
+# an Exact Target Pull action, then an '...option(1)... option(N)...'
+# format is used. SG password actions also have a qid with two option
+# fields. Here, we extract the "option 0" parameter from the questions
+# so they will merge correctly with the array of names.
+lc_names <- lapply(lc_names, gsub, patt= ", option(0)",
+                   rep="", fixed=TRUE)
+
+# Now remove the patterns saved above.
+lc_names <- lapply(lc_names, gsub, patt= patterns, rep="")
+
+# Save the SG geography variables derived from respondent IP
+if(keep_geo_vars) {
+  lc_names <- lapply(lc_names, gsub, patt="^variableLONG$",       rep="rsp_lng")
+  lc_names <- lapply(lc_names, gsub, patt="^variableLAT$",        rep="rsp_lat")
+  lc_names <- lapply(lc_names, gsub, patt="^variableGEOCOUNTRY$", rep="rsp_cntry")
+  lc_names <- lapply(lc_names, gsub, patt="^variableGEOCITY$",    rep="rsp_city")
+  lc_names <- lapply(lc_names, gsub, patt="^variableGEOREGION$",  rep="rsp_region")
+  lc_names <- lapply(lc_names, gsub, patt="^variableGEOPOSTAL$",  rep="rsp_post")
+}
+
+
+# Now rename SG system fields, subtituting "sys_" for "variable"
+lc_names <- lapply(lc_names, gsub, patt= "variable",
+                   rep="sys_")
+
+# Now lower case the names and convert to data.frame
+lc_names <- data.frame(id=tolower(lc_names))
+
+# Index to preserve the inital column sorting
+lc_names$index <- seq_len(nrow(lc_names))
+
+# Left join of the cleaned name table
+lc_names <- merge(lc_names, lc_qs, by="id", all.x=TRUE)
+lc_names <- lc_names[order(lc_names$index),]
+rownames(lc_names) <- NULL # reset the row.names attribute
+
+# Replace variable name with shortname wher available
+lc_names$id   <- ifelse(is.na(lc_names$shortname),
+                        lc_names$id, lc_names$shortname)
+
+# Subset the vector of cleaned names and replace the full_set names
+lc_names <- lc_names[,'id']
+colnames(lc_fullset) <- lc_names
+
+# Removes 'sys_*' vars that end in a digit
+# Removes residual number-only vars as they will falsely align in rbind
+drops <- "^[[:digit:]]+$|sys_[[:digit:]]+"
+set <- lc_fullset[, -grep(drops, names(lc_fullset), perl=TRUE)]
+
+if(delete_sys_vars | clean) set <- set[, -grep("^sys_", names(set),
+                                               ignore.case=TRUE)]
+
+# Other variables to drop per the "clean" parameter (V4 API)
+otherdrops <- paste0("contactid|istestdata|sessionid|",
+                     "language|ilinkid|sresponsecomment")
+
+if(clean) set <- set[, -grep(otherdrops, names(set), ignore.case=TRUE)]
+if(clean) set <- as.data.frame(lapply(set, type.convert, na.strings = "", as.is=TRUE))
+if(reset_row_names) row.names(set) <- NULL
+
+# Format Survey Gizmo date fields
+set[, c('datestarted', 'datesubmitted')] <- lapply(set[, c('datestarted', 'datesubmitted')],
+                                                   as.POSIXct, format="%Y-%m-%d")
+
+# Merge data from the contact object with survey returns
+if(mergecampaign) {
+  campaign <- Rsurveygizmo::pullsg_campaign(surveyid, api, small=small)
+  if(sum(duplicated(campaign$contactid))){
+    message("Campaign data for survey id# ", surveyid, " contains duplicate contactid records.")
+    message("The most recent record will be selected and merged with the survey data. \nHowever, we strongly, ",
+            "recommend that you merge campaign data separately after examining duplicate contactids by:\n",
+            "(1) Re-downloading the response data for survey ", surveyid, ", choosing the option 'mergecampaign=FALSE', and\n",
+            "(2) Running the 'pullsg_campaign' separately, merging records after analyzing and discarding duplicates.")
+    campaign <- campaign[order(campaign[, "contactid"], -as.numeric(campaign[, "dlastsent"])), ]
+    campaign <- campaign[!duplicated(campaign$contactid), ]
+  }
+  
+  if(!is.null(campaign))  set <- merge(campaign, set, by="contactid", all.y=TRUE)
+  if(is.null(campaign))   message('Because no email campaign(s) were located for survey "', surveyid, '", email addresses will not be merged with the survey returns.')
+}
